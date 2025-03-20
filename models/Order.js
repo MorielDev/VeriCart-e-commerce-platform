a4d26572c5936dc64652fd278da36bdac6564346
Saintmoriel <nimrodyusuf@gmail.com>
2025-03-20 19:25:33 +0100

    style: restructured the order structure

---
+++ models/Order.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import User from './User';
+
+const OrderSchema = new mongoose.Schema({
+    User: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true,
+        ref: User 
+    },
+    product: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',  // References the Product model
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1  // Ensures at least 1 item is ordered
+            }
+        }
+    ], 
+
+    totalPrice: { 
+        type: Number, 
+        required: true 
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Shipped', 'Delivered'],  // Restricts values
+        default: 'Pending'  //  All new orders start as "Pending"
+    },
+    createdAt: { 
+        type:Date, 
+        required: true
+    }
+});
+
+const Order = mongoose.model('Order', OrderSchema);
+export default Order;
