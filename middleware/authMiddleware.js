627f7a12c0f21cb2ce8c9127626450909f4d69f0
Saintmoriel <nimrodyusuf@gmail.com>
2025-03-20 12:17:08 +0100

    fix: Manage the token security

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,27 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 
-const authMiddleware = (req, res, next) => {
-const token = req.header('Authorization');
-if (!token) return res.status(401).json({ error: 'Access denied' });
-try {
-    // remove bearer
-    const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
-    req.user = decoded;
-    next(); // move to the next router
-    
-    } catch (error) {
-        res.status(401).json({ error: 'Invalid token' });
+const authMiddleware = async (req, res, next) => {
+    const token = req.header("Authorization");
+    if (!token) {
+        return res.status(403).json({ message: "Access denied. No token provided." });
+    }
+
+    try {
+        const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
         }
-        };
+
+        req.user = user;
+        next();
+    } catch (error) {
+        if (error.name === "TokenExpiredError") {
+            return res.status(401).json({ message: "Session expired. Please log in again." });
+        }
+        res.status(401).json({ message: "Invalid token" });
+    }
+};
 
 export default authMiddleware;
