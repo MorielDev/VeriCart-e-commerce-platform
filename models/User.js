5f49878f8874fec2e096ce5a97e4e51f09864908
Saintmoriel <nimrodyusuf@gmail.com>
2025-03-22 20:38:25 +0100

    Fix: Add admin option

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false }
 });
 
 const User = mongoose.model('User', UserSchema);
